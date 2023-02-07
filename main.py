5bc7c2f9f2f8b4db069cb0357f230af22009a892
Looper <aayush.bhattacharjee2002@gmail.com>
2023-02-07 16:20:32 +0530

    Made a update_task feature to update the task

--- main.py
+++ main.py
@@ -35,19 +35,39 @@ class Tasks:
             print("Do you want to update the task ")
             self.choice = input("Enter y/n: ")
             if self.choice == "y":
-                pass
+                Tasks.update_task(self, task, dt)
             else:
                 return
         else:
-            self.data[task] = dt.strftime("%d/%m/%Y | %H:%M:%S")
+            self.data[task] = dt
             with open("tasks.txt", "w") as f:
                 for key, value in self.data.items():
-                    f.write(f"{key} : {value}\n")
+                    f.write(f"{key}:{value}\n")
+
+    def update_task(self, task, dt):
+        self.data = Tasks.load_data()
+        print(self.data)
+        self.datakeys = self.data.keys()
+        if task not in self.datakeys:
+            print("Task does not exists")
+            print("Do you want to add the task ")
+            self.choice = input("Enter y/n: ")
+            if self.choice == "y":
+                Tasks.add_task(self, task, dt)
+            else:
+                return
+        else:
+            self.data[task] = dt
+            with open("tasks.txt", "w") as f:
+                for key, value in self.data.items():
+                    f.write(f"{key}:{value}\n")
 
 
 if __name__ == "__main__":
     test = Tasks()
     print(test.load_data())
-    test.add_task("test", datetime.datetime.now())
-    test.add_task("real task", datetime.datetime.now())
+    test.add_task("test", datetime.datetime.now().strftime(
+        "%d/%m/%Y | %H:%M:%S"))
+    test.add_task("real task", "09/02/2023 | 09:33:35")
+    test.update_task("test", "04/12/2023 | 10:33:35")
     print(test.load_data())
