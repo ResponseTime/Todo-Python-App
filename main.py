e16c7b384ee93a9823fe96cc21a82d9d0d95521a
Looper <aayush.bhattacharjee2002@gmail.com>
2023-02-07 09:34:22 +0530

    Created a add task feature

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import datetime
 
 
 class Tasks:
-    data_dict = dict()
 
     def __init__(self):
         if os.path.exists("tasks.txt"):
@@ -12,21 +11,43 @@ class Tasks:
         else:
             os.mkdir("tasks.txt")
 
-    def load_data(self):
-        with open("tasks.txt", "r") as f:
-            while True:
-                line = f.readline().rstrip('\n')
-                if line == "":
-                    break
-                else:
-                    Tasks.data_dict[line[:line.index(
-                        ':')]] = line[line.index(':') + 1:]
-        return Tasks.data_dict
+    @staticmethod
+    def load_data():
+        if os.path.exists("tasks.txt"):
+            data_dict = dict()
+            with open("tasks.txt", "r") as f:
+                while True:
+                    line = f.readline().rstrip('\n')
+                    if line == "":
+                        break
+                    else:
+                        data_dict[line[:line.index(
+                            ':')]] = line[line.index(':') + 1:]
+            return data_dict
+        else:
+            print("No file init object of class Tasks")
 
-    def add_task(self, task, date, time):
-        pass
+    def add_task(self, task, dt):
+        self.data = Tasks.load_data()
+        self.datakeys = self.data.keys()
+        if task in self.datakeys:
+            print("Task already exists")
+            print("Do you want to update the task ")
+            self.choice = input("Enter y/n: ")
+            if self.choice == "y":
+                pass
+            else:
+                return
+        else:
+            self.data[task] = dt.strftime("%d/%m/%Y | %H:%M:%S")
+            with open("tasks.txt", "w") as f:
+                for key, value in self.data.items():
+                    f.write(f"{key} : {value}\n")
 
 
 if __name__ == "__main__":
     test = Tasks()
+    print(test.load_data())
+    test.add_task("test", datetime.datetime.now())
+    test.add_task("real task", datetime.datetime.now())
     print(test.load_data())
